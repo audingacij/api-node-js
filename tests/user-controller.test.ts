c5edf89c12d82db77eef60f9a0866505703210b7
Audinga.Cijunaityte <audingacij@gmail.com>
2025-11-07 17:12:02 +0200

    completed tests

--- tests/user-controller.test.ts
+++ tests/user-controller.test.ts
@@ -12,24 +12,41 @@ test.describe('User management API', () => {
     });
 
     test('find user: should return a user by ID', async ({ request }) => {
-
+        //create a user
+        const response = await request.post(`${baseURL}`);
+        const responseBody = await response.json()
+        const userId = responseBody.id;
+        //find the user by id
+        const getResponse = await request.get(baseURL + '/' + userId);
+        expect(getResponse.status()).toBe(200);
     });
 
     test('find user: should return 404 if user not found', async ({ request }) => {
-
+        const nonExistingUserId = 999999999
+        const getResponse = await request.get(baseURL + '/' + nonExistingUserId);
+        expect(getResponse.status()).toBe(404);
     });
 
     test('create user: should add a new user', async ({ request }) => {
-
+        const response = await request.post(`${baseURL}`);
+        expect(response.status()).toBe(201);
+        const responseBody = await response.json()
+        expect(responseBody.name).toBeDefined;
+        console.log(responseBody);
     });
 
     test('delete user: should delete a user by ID', async ({ request }) => {
-
+        const response = await request.post(`${baseURL}`);
+        const responseBody = await response.json()
+        const userId = responseBody.id;
+        const deleteResponse  = await request.delete(baseURL + '/' + userId);
+        expect(deleteResponse .status()).toBe(200);
+        console.log(await deleteResponse .json());
     });
 
     test('delete user: should return 404 if user not found', async ({ request }) => {
-
+        const nonExistingUserId = 999999999;
+        const deleteResponse = await request.get(baseURL + '/' + nonExistingUserId);
+        expect(deleteResponse.status()).toBe(404);
     });
-
-
 });
